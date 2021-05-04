3f35a63a8c5ab2b8070bac4f0c8a73cdb9fb6538
hkt.zaradev <hkt.zaradev@gmail.com>
2021-05-04 12:28:52 +1200

    Added popover component.
    When a user hovers a the project ID in the card it will display the project description in a popover tooltip.

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,7 +1,15 @@
 import React from 'react';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
+import Popover from 'react-bootstrap/Popover';
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger'
 import '../css/ProjectCard.css';
+
+const popover = (description) => (
+    <Popover id="popover-basic">
+      <Popover.Content>{description}</Popover.Content>
+    </Popover>
+);
 
 // https://blog.bitsrc.io/add-a-simple-search-function-to-react-app-without-a-server-22deda8966cd
 const ProjectCard = ({projectName, description, projectIdentifier, start_date, end_date}) => {
@@ -10,7 +18,9 @@ const ProjectCard = ({projectName, description, projectIdentifier, start_date, e
         <Card className='project-entry'>
             <Card.Body>
                 <Button variant='danger'>X</Button>
-                <Card.Title>Project {projectIdentifier}</Card.Title>
+                <OverlayTrigger trigger='hover' placement='right' overlay={popover(description)}>
+                    <Card.Title>Project {projectIdentifier}</Card.Title>
+                </OverlayTrigger>
                 <Card.Subtitle className='mb-2 text-muted'>{projectName}</Card.Subtitle>
                 <Card.Text className='text-dates end-date'>{end_date}</Card.Text>
                 <Card.Text className='text-dates start-date'>{start_date}</Card.Text>
