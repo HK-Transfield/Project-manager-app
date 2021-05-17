6f2a1ed20331be7d1eacbe8c27c93e618a9f9836
HK Transfield <h.k.transfield1@gmail.com>
2021-05-17 12:11:32 +1200

    Converted CreateProjectForm.js to be a functional component.

--- src/components/CreateProjectForm.js
+++ src/components/CreateProjectForm.js
@@ -1,29 +1,20 @@
-import React from "react";
+import React, { useState, useRef, isValidElement } from "react";
 import Form from 'react-bootstrap/Form';
 import FormControl from 'react-bootstrap/FormControl';
 import Button from 'react-bootstrap/Button';
-import { connect } from 'react-redux';
+import { connect, useDispatch } from 'react-redux';
 import '../css/CreateProjectForm.css';
 import '../css/DisplayModal.css';
 
 /**
- * Default state when the project manager loads.
- * No values are stored.
- * 
- * @returns The default state of the component.
+ * Used to set the field state to have no values.
  */
-const initialState = () => {
-    return  {
-        isValid: false,
-        fields: {
-            projectName: "",
-            projectIdentifier: "",
-            description: "",
-            start_date: "",
-            end_date: ""
-        },
-        errors: {}
-    }
+const initialState = {
+    projectName: "",
+    projectIdentifier: "",
+    description: "",
+    start_date: "",
+    end_date: ""
 }
 
 
@@ -37,29 +28,56 @@ const initialState = () => {
  * 
  * @author Harmon Transfield
  */
-class CreateProjectForm extends React.Component {
-    constructor(props) {
-        super(props);
+const CreateProjectForm = () => {
 
-        this.state = initialState();
-        this.projectFormRef = React.createRef();
-        
-        this.handleSubmit = this.handleSubmit.bind(this);
-        this.handleChange = this.handleChange.bind(this);
+    /**********************
+     * Declare Hooks
+     **********************/
+    const dispatch = useDispatch();
+    const projectFormRef = useRef(null);
+    const [valid, setValid] = useState(false);
+    const [fields, setFields] = useState(initialState);
+    const[errors, setErrors] = useState({});
+
+
+    /**********************
+     * Component Functions
+     **********************/
+
+    /**
+     * Resets the forms to show no values in form controls.
+     */
+    const handleReset = () => {
+        projectFormRef.current.reset();
+        setFields(initialState);
+        setValid(false);
     }
 
     /**
-     * Checks that all form controls contain valid input.
+     * Updates the component's field state to contain the input of the user.
+     * 
+     * @param {event} event The user inputting data into the form.
+     */
+    const handleChange = (event) => {
+        let {name, value} = event.target;
+
+        setFields({...fields, [name]:value});
+    }
+
+    /**
+     * Checks that all form controls contain valid input. If any 
+     * of the inputs are invalid this function will generate the 
+     * the appropriate error message, which will then be
+     * displayed underneath the form control.
      * 
      * @returns A boolean representing the validity of the form.
      */
-    validate = () => {
-        let fields = this.state.fields;
+    const validate = () => {
         let errors = {};
 
-        // projectName validation
-        if(!fields['projectName']) 
-            errors['projectNameError'] = "Project name cannot be blank.";
+         // projectName validation
+         if(!fields['projectName']) 
+         errors['projectNameError'] = "Project name cannot be blank.";
 
         // project ID validation
         if(!fields['projectIdentifier']) 
@@ -82,27 +100,13 @@ class CreateProjectForm extends React.Component {
 
         // there are errors in the form
         if(Object.keys(errors).length > 0) {
-            this.setState({errors});
+            console.log('nooooooooooo')
+            console.log(errors)
+            setErrors(errors);
             return false;
         }
 
-        return true;
-    }
-
-    /**
-     * Updates the component's field state to contain the input of the user.
-     * 
-     * @param {event} event The user inputting data into the form.
-     */
-     handleChange = (event) => {  
-        const {name, value} = event.target;
-
-        // update fields state with user input
-        this.setState(
-            {
-                fields: { ...this.state.fields, [name]: value }
-            }
-        );
+     return true;
     }
 
     /**
@@ -111,137 +115,132 @@ class CreateProjectForm extends React.Component {
      * 
      * @param {event} event User clicks on the submit button 
      */
-    handleSubmit = (event) => {
-        event.preventDefault(); // cancels Event (Stops HTML Default Form Submit)
+    const handleSubmit = (event) => {
+        // Stop the HTML default form submit
+        event.preventDefault();
         
-        const form = event.currentTarget;
-        console.log('form validation: ' + this.validate())
-
-        // update state to call bootstrap form validation
-        this.setState({isValid: this.validate()});
-
-        if (form.checkValidity() === false) {
-            event.stopPropagation(); // prevents Event Bubbling To Parent Elements
-        }  else {
-            // store the new project
-            this.props.dispatch({
-                type: "ADD_PROJECT",
-                payload: this.state.fields
+        // get the form so that we can check validity
+        let form = event.currentTarget;
+        
+        // perform validation check
+        setValid(validate());
+        
+        if (form.checkValidity() === false) { // check form validity after setting the state
+            event.stopPropagation();
+        }
+        else {
+            // send project to store
+            dispatch({
+                type: 'ADD_PROJECT',
+                payload: fields
             });
-
-            // clear form
-            this.handleReset();
+            handleReset();
         }
     }
 
-    /**
-     * Resets the forms to show no values in form controls.
-     */
-    handleReset = () => {
-        this.projectFormRef.current.reset();
-        this.setState({isValid: false});
-    }
 
-    render() {
-        return(
-           <div className='form-container'>
-                <div className='form-header-container'>
-                    <h1 className='form-header'>Create Project</h1>
-                </div>
-                <hr/>
-                <Form 
-                    noValidate 
-                    validated={this.state.isValid} 
-                    onSubmit={this.handleSubmit}
-                    ref={this.projectFormRef}
+    /**********************
+     * Return DOM Elements
+     **********************/
+    return(
+        <div className='form-container'>
+            <div className='form-header-container'>
+                <h1 className='form-header'>Create Project</h1>
+            </div>
+            <hr/>
+            <Form 
+                noValidate 
+                validated={valid} 
+                onSubmit={handleSubmit}
+                ref={projectFormRef}
+            >
+                <Form.Group controlId='formProjectDetails'>
+
+                    {/*|| Project name validation */}
+                    <Form.Control 
+                        required
+                        name='projectName'
+                        type='text'
+                        placeholder='Project Name' 
+                        onChange={handleChange}
+                        isInvalid={!!errors['projectNameError']}
+                    />
+                    <FormControl.Feedback type="invalid">
+                        {errors['projectNameError']}
+                    </FormControl.Feedback>
+                    <br/>
+                    
+                    {/*|| Project identifier validation */}
+                    <Form.Control 
+                        required
+                        name='projectIdentifier'
+                        type='text'
+                        placeholder='Project ID' 
+                        onChange={handleChange}
+                        isInvalid={!!errors['projectIdentifierError']}
+                    />
+                        <FormControl.Feedback type="invalid">
+                        {errors['projectIdentifierError']}
+                    </FormControl.Feedback>
+                    <br/>
+                    
+                    {/*|| Project description validation */}
+                    <Form.Control 
+                        required
+                        name='description'
+                        as='textarea'
+                        placeholder='Project Description'
+                        onChange={handleChange} 
+                        isInvalid={!!errors['descriptionError']}
+                    />
+                    <FormControl.Feedback type="invalid">
+                        {errors['descriptionError']}
+                    </FormControl.Feedback>
+                </Form.Group>
+
+                    
+                <Form.Group controlId='formProjectDates'>
+
+                    {/*|| Project start date validation*/}
+                    <Form.Label>Start Date</Form.Label>
+                    <Form.Control 
+                        required
+                        name='start_date'
+                        type='date' 
+                        placeholder='dd/mm/yyyy' 
+                        onChange={handleChange}
+                        isInvalid={!!errors['startDateError']}
+                    />
+                    <FormControl.Feedback type="invalid">
+                        {errors['startDateError']}
+                    </FormControl.Feedback>
+                    <br/>
+                    
+                    {/*|| Project end date validation*/}
+                    <Form.Label>End Date</Form.Label>
+                    <Form.Control 
+                        required
+                        name='end_date'
+                        type='date' 
+                        placeholder='dd/mm/yyyy'
+                        onChange={handleChange}
+                        isInvalid={!!errors['endDateError']}
+                    />
+                    <FormControl.Feedback type="invalid">
+                        {errors['endDateError']}
+                    </FormControl.Feedback>
+                </Form.Group>
+                <Button 
+                    type='submit'
+                    className='btn-submit-project' 
+                    variant='dark' 
+                    block
                 >
-                    <Form.Group controlId='formProjectDetails'>
+                    Create Project
+                </Button>
+            </Form>
+        </div>
+    );
 
-                        {/*|| Project name validation */}
-                        <Form.Control 
-                            required
-                            name='projectName'
-                            type='text'
-                            placeholder='Project Name' 
-                            onChange={this.handleChange}
-                            isInvalid={!!this.state.errors['projectNameError']}
-                        />
-                        <FormControl.Feedback type="invalid">
-                            {this.state.errors['projectNameError']}
-                        </FormControl.Feedback>
-                        <br/>
-                        
-                        {/*|| Project identifier validation */}
-                        <Form.Control 
-                            required
-                            name='projectIdentifier'
-                            type='text'
-                            placeholder='Project ID' 
-                            onChange={this.handleChange}
-                            isInvalid={!!this.state.errors['projectIdentifierError']}
-                        />
-                         <FormControl.Feedback type="invalid">
-                            {this.state.errors['projectIdentifierError']}
-                        </FormControl.Feedback>
-                        <br/>
-                        
-                        {/*|| Project description validation */}
-                        <Form.Control 
-                            required
-                            name='description'
-                            as='textarea'
-                            placeholder='Project Description'
-                            onChange={this.handleChange} 
-                            isInvalid={!!this.state.errors['descriptionError']}
-                        />
-                        <FormControl.Feedback type="invalid">
-                            {this.state.errors['descriptionError']}
-                        </FormControl.Feedback>
-                    </Form.Group>
-
-                        
-                    <Form.Group controlId='formProjectDates'>
-
-                        {/*|| Project start date validation*/}
-                        <Form.Label>Start Date</Form.Label>
-                        <Form.Control 
-                            required
-                            name='start_date'
-                            type='date' 
-                            placeholder='dd/mm/yyyy' 
-                            onChange={this.handleChange}
-                            isInvalid={!!this.state.errors['startDateError']}
-                        />
-                        <FormControl.Feedback type="invalid">
-                            {this.state.errors['startDateError']}
-                        </FormControl.Feedback>
-                        <br/>
-                        
-                        {/*|| Project end date validation*/}
-                        <Form.Label>End Date</Form.Label>
-                        <Form.Control 
-                            required
-                            name='end_date'
-                            type='date' 
-                            placeholder='dd/mm/yyyy'
-                            onChange={this.handleChange}
-                            isInvalid={!!this.state.errors['endDateError']}
-                        />
-                        <FormControl.Feedback type="invalid">
-                            {this.state.errors['endDateError']}
-                        </FormControl.Feedback>
-                    </Form.Group>
-                    <Button 
-                        type='submit'
-                        className='btn-submit-project' 
-                        variant='dark' 
-                        block
-                    >
-                        Create Project
-                    </Button>
-                </Form>
-           </div>
-        ); 
-    }
 }
 export default connect(null)(CreateProjectForm);
--- src/components/CreateProjectModal.js
+++ src/components/CreateProjectModal.js
@@ -4,7 +4,7 @@ import Modal from 'react-bootstrap/Modal';
 import '../css/CreateProjectForm.css';
 import '../css/DisplayModal.css';
 import CreateProjectForm from './CreateProjectForm';
-// https://stackoverflow.com/questions/56107329/react-how-to-close-a-modal-from-child-opened-from-parent-component
+
  /**
   * Uses a bootstrap modal to display a create project form.
   * This can be opened with a button
@@ -12,12 +12,13 @@ import CreateProjectForm from './CreateProjectForm';
   * @returns The DOM containing the modal and form 
   */
 const CreateProjectModal = () => {
+    // declare state using hooks
     const [showModal, setShowModal] = useState(false);
 
+    // define functions to handle state
     const handleCloseModal = () => setShowModal(false);
     const handleShowModal = () => setShowModal(true);
 
-    // https://www.pluralsight.com/guides/how-to-open-and-close-react-bootstrap-modal-programmatically
     return(
         <div>
             <Button  
